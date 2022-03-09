a10a3c5fe17689a7fec3b7478f8a6fc2ff6603bd
Dambert <dmsantillana2705@gmail.com>
2022-03-09 16:14:50 -0500

    [agregarMetodo] - new method in string extension

---
+++ CompositeLayout/CompositeLayout/Util.swift
@@ -0,0 +1,12 @@
+//
+//  Util.swift
+//  CompositeLayout
+//
+//  Created by Dambert Louis Munoz Santillana on 9/03/22.
+//
+
+import Foundation
+
+extension String {
+    static var name: String = "Name"
+}
